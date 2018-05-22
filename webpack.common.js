585784d2f5bcca1170a77ef2bca023aece184c40
ruslan <afro.funky.lover@gmail.com>
2018-05-22 22:23:51 +0700

    add pug support

--- webpack.common.js
+++ webpack.common.js
@@ -6,9 +6,17 @@ module.exports = {
     entry: {
         app: './src/index.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loaders: ['pug-loader']
+            }
+        ]
+    },
     plugins: [
         new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin({ template: './src/index.pug', })
     ],
     output: {
         filename: '[name].bundle.js',
